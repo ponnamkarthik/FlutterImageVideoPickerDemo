3efadfca2d74a9851605923cebb6cbae49a1b9fc
Ponnam Karthik <ponnamkarthik3@gmail.com>
2020-10-28 07:00:04 +0530

    Repo Updated to latest plugins

--- android/app/src/main/kotlin/com/example/bootcamp_image_video_picker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bootcamp_image_video_picker/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.bootcamp_image_video_picker
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -25,55 +25,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   File _image;
   File _cameraImage;
   File _video;
   File _cameraVideo;
+
+  ImagePicker picker = ImagePicker();
 
   VideoPlayerController _videoPlayerController;
   VideoPlayerController _cameraVideoPlayerController;
 
   // This funcion will helps you to pick and Image from Gallery
   _pickImageFromGallery() async {
-    File image = await  ImagePicker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+    PickedFile pickedFile =
+        await picker.getImage(source: ImageSource.gallery, imageQuality: 50);
+
+    File image = File(pickedFile.path);
 
     setState(() {
-      _image = image;    
+      _image = image;
     });
   }
 
-  
   // This funcion will helps you to pick and Image from Camera
   _pickImageFromCamera() async {
-    File image = await  ImagePicker.pickImage(source: ImageSource.camera, imageQuality: 50);
+    PickedFile pickedFile =
+        await picker.getImage(source: ImageSource.camera, imageQuality: 50);
+
+    File image = File(pickedFile.path);
 
     setState(() {
-      _cameraImage = image;    
+      _cameraImage = image;
     });
   }
 
-  
   // This funcion will helps you to pick a Video File
   _pickVideo() async {
-    File video = await ImagePicker.pickVideo(source: ImageSource.gallery);
-     _video = video; 
-    _videoPlayerController = VideoPlayerController.file(_video)..initialize().then((_) {
-      setState(() { });
-      _videoPlayerController.play();
-    });
+    PickedFile pickedFile = await picker.getVideo(source: ImageSource.gallery);
+
+    _video = File(pickedFile.path);
+
+    _videoPlayerController = VideoPlayerController.file(_video)
+      ..initialize().then((_) {
+        setState(() {});
+        _videoPlayerController.play();
+      });
   }
 
-   // This funcion will helps you to pick a Video File from Camera
+  // This funcion will helps you to pick a Video File from Camera
   _pickVideoFromCamera() async {
-    File video = await ImagePicker.pickVideo(source: ImageSource.camera);
-     _cameraVideo = video; 
-    _cameraVideoPlayerController = VideoPlayerController.file(_cameraVideo)..initialize().then((_) {
-      setState(() { });
-      _cameraVideoPlayerController.play();
-    });
+    PickedFile pickedFile = await picker.getVideo(source: ImageSource.camera);
+
+    _cameraVideo = File(pickedFile.path);
+
+    _cameraVideoPlayerController = VideoPlayerController.file(_cameraVideo)
+      ..initialize().then((_) {
+        setState(() {});
+        _cameraVideoPlayerController.play();
+      });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,12 +96,14 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Container(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-
               children: <Widget>[
-                if(_image != null) 
+                if (_image != null)
                   Image.file(_image)
                 else
-                  Text("Click on Pick Image to select an Image", style: TextStyle(fontSize: 18.0),),
+                  Text(
+                    "Click on Pick Image to select an Image",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
                 RaisedButton(
                   onPressed: () {
                     _pickImageFromGallery();
@@ -100,40 +113,50 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   height: 16.0,
                 ),
-                if(_cameraImage != null) 
+                if (_cameraImage != null)
                   Image.file(_cameraImage)
                 else
-                  Text("Click on Pick Image to select an Image", style: TextStyle(fontSize: 18.0),),
+                  Text(
+                    "Click on Pick Image to select an Image",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
                 RaisedButton(
                   onPressed: () {
                     _pickImageFromCamera();
                   },
                   child: Text("Pick Image From Camera"),
                 ),
-                if(_video != null) 
-                      _videoPlayerController.value.initialized
-                  ? AspectRatio(
-                      aspectRatio: _videoPlayerController.value.aspectRatio,
-                      child: VideoPlayer(_videoPlayerController),
-                    )
-                  : Container()
+                if (_video != null)
+                  _videoPlayerController.value.initialized
+                      ? AspectRatio(
+                          aspectRatio: _videoPlayerController.value.aspectRatio,
+                          child: VideoPlayer(_videoPlayerController),
+                        )
+                      : Container()
                 else
-                  Text("Click on Pick Video to select video", style: TextStyle(fontSize: 18.0),),
+                  Text(
+                    "Click on Pick Video to select video",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
                 RaisedButton(
                   onPressed: () {
                     _pickVideo();
                   },
                   child: Text("Pick Video From Gallery"),
                 ),
-                if(_cameraVideo != null) 
-                      _cameraVideoPlayerController.value.initialized
-                  ? AspectRatio(
-                      aspectRatio: _cameraVideoPlayerController.value.aspectRatio,
-                      child: VideoPlayer(_cameraVideoPlayerController),
-                    )
-                  : Container()
+                if (_cameraVideo != null)
+                  _cameraVideoPlayerController.value.initialized
+                      ? AspectRatio(
+                          aspectRatio:
+                              _cameraVideoPlayerController.value.aspectRatio,
+                          child: VideoPlayer(_cameraVideoPlayerController),
+                        )
+                      : Container()
                 else
-                  Text("Click on Pick Video to select video", style: TextStyle(fontSize: 18.0),),
+                  Text(
+                    "Click on Pick Video to select video",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
                 RaisedButton(
                   onPressed: () {
                     _pickVideoFromCamera();
